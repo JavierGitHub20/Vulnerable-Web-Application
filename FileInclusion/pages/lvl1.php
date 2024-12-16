01c51717c0d8c163946b1beb864a0a964b3f95d0
JavierGitHub20 <74763683+JavierGitHub20@users.noreply.github.com>
2024-12-16 16:58:24 +0100

    Update lvl1.php

--- FileInclusion/pages/lvl1.php
+++ FileInclusion/pages/lvl1.php
@@ -22,7 +22,8 @@
         
         if (isset( $_GET[ 'file' ]))        
         {
-          @include($_GET[ 'file' ]);
+          //@include($_GET[ 'file' ]);
+           
           echo"<div align='center'><b><h5>".$_GET[ 'file' ]."</h5></b></div> ";       
         }
       ?>
