21594e6b550734a90efab7cfcd03722accafe4ba
JavierGitHub20 <74763683+JavierGitHub20@users.noreply.github.com>
2024-12-16 17:10:11 +0100

    Update lvl4.php

--- FileInclusion/pages/lvl4.php
+++ FileInclusion/pages/lvl4.php
@@ -25,17 +25,15 @@
         {
           $secure4 = $_GET[ 'file' ];
          
-            if ($secure4!="1.php" && $secure4!="2.php") 
-            {
-              $secure4=substr($secure4, 0,-4);
+            if ($secure4!="1.php" && $secure4!="2.php") {
+                    $secure4=substr($secure4, 0,-4);
 
-               if (isset($secure4)) 
-            {        
-              include($secure4);              
-            }
+                if (isset($secure4)) {        
+                    include($secure4);              
+               }  
+              
             }
            
-             
         }              
       ?>
    </body>
