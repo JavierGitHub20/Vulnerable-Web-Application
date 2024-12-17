7d921b26f16716c558a26fb4acca256f53cdc052
JavierGitHub20 <74763683+JavierGitHub20@users.noreply.github.com>
2024-12-17 20:52:26 +0100

    Update sql4.php

--- SQL/sql4.php
+++ SQL/sql4.php
@@ -49,9 +49,19 @@
 			exit;
 		}
 
-		$query = "SELECT bookname,authorname FROM books WHERE number =?"; 
+		$query = "SELECT bookname,authorname FROM books WHERE number =?";
 
-$stmt = mysqli_prepare($conn, $sql);
+		$stmt = mysqli_prepare($conn, $sql);
+
+// Vincular parámetros (sanitización automática)
+mysqli_stmt_bind_param($stmt, "s", $number);
+
+// Ejecutar la consulta
+mysqli_stmt_execute($stmt);
+
+// Obtener resultados
+$resultado = mysqli_stmt_get_result($stmt);
+
 
 // Vincular parámetros (sanitización automática)
 mysqli_stmt_bind_param($stmt, "s", $number);
